a1c2710b8ebce71272455b894d99ec5b8f789208
aborgeat01 <you@example.com>
2022-08-30 20:04:49 -0300

    agrego patente y modelo2

--- src/ar/unlm/gittn/Auto.java
+++ src/ar/unlm/gittn/Auto.java
@@ -1,5 +1,13 @@
 package ar.unlm.gittn;
 
 public class Auto {
+	private String modelo;
+	private int patente;
+	public Auto(String modelo, int patente) {
+	
+		this.modelo = modelo;
+		this.patente = patente;
+	}
+	
 
 }
